460dfb13e024a8d9d0c45fea989448f6caa59c4d
Mark Hendrickson <markmhendrickson@gmail.com>
2014-04-04 15:16:55 -0700

    Add endpoint for Dropbox account info retrieval

--- dropbox.js
+++ dropbox.js
@@ -1,5 +1,6 @@
 module.exports = function(app, passport) {
   var dropboxPassport = require('passport-dropbox-oauth2');
+  var https = require('https');
 
   passport.use(new dropboxPassport.Strategy({
       clientID: process.env.ASHEVILLE_SYNC_DROPBOX_APP_KEY,
@@ -7,7 +8,13 @@ module.exports = function(app, passport) {
       callbackURL: 'http://localhost:9090/storages/dropbox/auth-callback'
     },
     function(accessToken, refreshToken, profile, done) {
-      return done(null, { storages: { dropbox: { token: accessToken } } });
+      return done(null, { 
+        storages: { 
+          dropbox: { 
+            token: accessToken 
+          } 
+        } 
+      });
     }
   ));
 
@@ -16,10 +23,72 @@ module.exports = function(app, passport) {
   app.get('/storages/dropbox/auth-callback', passport.authenticate('dropbox-oauth2', { 
     failureRedirect: '/login'
   }), function(req, res) {
-    res.redirect('/storages/dropbox');
+    if (req.session.dropboxAuthRedirect) {
+      res.redirect(req.session.dropboxAuthRedirect);
+      req.session.dropboxAuthRedirect = null;
+    } else {
+      res.redirect('/storages/dropbox');
+    }
   });
 
   app.get('/storages/dropbox', function(req, res) {
-    res.json({ storages: { dropbox: { token: req.user.storages.dropbox.token } } });
+    if (typeof req.user == 'undefined' || !req.user.storages.dropbox.token) {
+      req.session.dropboxAuthRedirect = '/storages/dropbox';
+      res.redirect('/storages/dropbox/auth');
+      return;
+    }
+
+    res.json({ 
+      storages: { 
+        dropbox: { 
+          token: req.user.storages.dropbox.token 
+        } 
+      } 
+    });
+  });
+
+  app.get('/storages/dropbox/account/info', function(req, res) {
+    if (typeof req.user == 'undefined' || !req.user.storages.dropbox.token) {
+      req.session.dropboxAuthRedirect = '/storages/dropbox/account/info';
+      res.redirect('/storages/dropbox/auth');
+      return;
+    }
+
+    var options = {
+      host: 'api.dropbox.com',
+      path: '/1/account/info?access_token=' + req.user.storages.dropbox.token
+    };
+
+    console.log('options', options);
+
+    _res = res;
+
+    try {
+      var req = https.get(options, function(res) {
+        try {
+          if (res.statusCode == 401) {
+            throw { message: 'Unauthorized request' };
+          }
+
+          var data = '';
+
+          res.on('data', function(chunk) {
+            data += chunk;
+          });
+
+          res.on('end', function() {
+            console.log('end!', data);
+            var json = JSON.parse(data);
+            _res.json({ response: json });
+          });
+        } catch (e) {
+          _res.json({ error: 'Response error: ' + e });
+        }
+      }).on('error', function(e) {
+        throw { message: 'Request error: ' + e };
+      });
+    } catch (e) {
+      res.json({ error: e.message });
+    }
   });
 }
