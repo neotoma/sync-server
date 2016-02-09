a9dceda2dee0fa7c6516bf2fa530194e138b647b
Mark Hendrickson <markmhendrickson@gmail.com>
2016-02-09 13:45:39 +0100

    Refactor content type model and controller
    - Move toObject functionality into model from controller
    - Add name and pluralName attributes to model
    - Create prototype lib with string methods

--- controllers/content_types.js
+++ controllers/content_types.js
@@ -1,48 +1,20 @@
+var ContentType = require('../models/contentType');
+
 module.exports = {
   toObject: function(sources) {
-    return [
-      {
-        id: 'checkin',
-        name: 'Checkin',
-        pluralName: 'Checkins',
-        sources: this.sourceIds(sources, 'checkin')
-      },
-      {
-        id: 'friend',
-        name: 'Friend',
-        pluralName: 'Friends',
-        sources: this.sourceIds(sources, 'friend')
-      },
-      {
-        id: 'tip',
-        name: 'Tip',
-        pluralName: 'Tips',
-        sources: this.sourceIds(sources, 'tip')
-      },
-      {
-        id: 'image',
-        name: 'Image',
-        pluralName: 'Images',
-        sources: this.sourceIds(sources, 'image')
-      },
-      {
-        id: 'video',
-        name: 'Video',
-        pluralName: 'Videos',
-        sources: this.sourceIds(sources, 'video')
-      }
+    var contentTypeIds = [
+      'checkin',
+      'friend',
+      'image',
+      'tip',
+      'video'
     ];
-  },
 
-  sourceIds: function(sources, contentTypeId) {
-    var sourceIds = [];
-
-    sources.forEach(function(source) {
-      if (source.contentTypes && source.contentTypes.indexOf(contentTypeId) > -1) {
-        sourceIds.push(source.id);
-      }
+    var contentTypeObjects = [];
+    contentTypeIds.forEach(function(contentTypeId) {
+      contentTypeObjects.push(new ContentType(contentTypeId).toObject(sources));
     });
 
-    return sourceIds;
+    return contentTypeObjects;
   }
 };
--- models/contentType.js
+++ models/contentType.js
@@ -3,4 +3,28 @@ var pluralize = require('pluralize');
 module.exports = function ContentType(id) {
   this.id = id;
   this.plural_id = pluralize(id);
+  this.name = id.capitalizeFirstLetter();
+  this.plural_name = this.plural_id.capitalizeFirstLetter();
+
+  this.toObject = function(sources) {
+    var sourceIds;
+    var self = this;
+
+    if (typeof sources === 'array' && sources.length > 0) {
+      sourceIds = [];
+      sources.forEach(function(source) {
+        if (source.contentTypes && source.contentTypes.indexOf(self.id) > -1) {
+          sourceIds.push(source.id);
+        }
+      });
+    }
+
+    return {
+      id: this.id,
+      pluralId: this.plural_id,
+      name: this.name,
+      pluralName: this.plural_name,
+      sourceIds: sourceIds
+    };
+  };
 }
