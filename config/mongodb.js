0a701226ea41d999953de6e406810c85796a624c
Mark Hendrickson <markmhendrickson@gmail.com>
2017-02-28 16:44:01 +0100

    Improve error handling on server run
    - Add .env file watching to Gruntfile watch
    - Regenerate JSDoc documentation

--- config/mongodb.js
+++ config/mongodb.js
@@ -5,23 +5,18 @@
 
 var logger = require('../lib/logger');
 
-try {
-  if (!process.env.SYNC_SERVER_MONGODB_DATABASE) {
-    throw new Error('MongoDB not provided database name');
-  }
-
-  if (!process.env.SYNC_SERVER_MONGODB_HOST) {
-    throw new Error('MongoDB failed to find host variable environment');
-  }
-
-  if (!process.env.SYNC_SERVER_MONGODB_PORT) {
-    throw new Error('MongoDB port not provided by environment');
-  }
-
-  module.exports = {
-    url: 'mongodb://' + process.env.SYNC_SERVER_MONGODB_HOST + ':' + process.env.SYNC_SERVER_MONGODB_PORT + '/' + process.env.SYNC_SERVER_MONGODB_DATABASE
-  };
-} catch(error) {
-  logger.fatal(error.message);
-  throw error;
+if (!process.env.SYNC_SERVER_MONGODB_DATABASE) {
+  throw new Error('MongoDB not provided database name');
 }
+
+if (!process.env.SYNC_SERVER_MONGODB_HOST) {
+  throw new Error('MongoDB failed to find host variable environment');
+}
+
+if (!process.env.SYNC_SERVER_MONGODB_PORT) {
+  throw new Error('MongoDB port not provided by environment');
+}
+
+module.exports = {
+  url: 'mongodb://' + process.env.SYNC_SERVER_MONGODB_HOST + ':' + process.env.SYNC_SERVER_MONGODB_PORT + '/' + process.env.SYNC_SERVER_MONGODB_DATABASE
+};
